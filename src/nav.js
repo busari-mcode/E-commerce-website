176806b2441aaec26fefa87a82ddb834f7a66753
Busari Moruf <busarimoruf20@gmail.com>
2023-09-27 00:24:00 +0100

    new changes

---
+++ src/nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { FaTruckMoving } from 'react-icons/fa';
+const Nav = () => {
+  return (
+    <>
+    <div className='free'>
+        <p><FaTruckMoving /> Free shipping when shopping upto $1000</p>
+    </div>
+    </>
+  )
+}
+
+export default Nav
