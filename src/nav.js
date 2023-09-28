e59e89578f1cbfc59bb43f2f372ef936c86688d9
Busari Moruf <busarimoruf20@gmail.com>
2023-09-28 23:59:26 +0100

    new changes

--- src/nav.js
+++ src/nav.js
@@ -4,6 +4,7 @@ import { AiOutlineHeart } from 'react-icons/ai';
 import { BsBagCheck } from 'react-icons/bs';
 import { AiOutlineUser } from 'react-icons/ai';
 import { CiLogin } from 'react-icons/ci';
+import { CiLogout } from 'react-icons/ci';
 import { Link } from 'react-router-dom';
 import './nav.css';
 const Nav = () => {
@@ -40,22 +41,25 @@ const Nav = () => {
     </div>
     <div className='header'>
       <div className='container'>
+        <div className='nav'>
         <ul>
           <li>
-            <Link to='/'>Home</Link>
+            <Link to='/' className='link'>Home</Link>
           </li>
           <li>
-            <Link to='/product'>Product</Link>
+            <Link to='/product' className='link'>Product</Link>
           </li>
           <li>
-            <Link to='/about'>About</Link>
+            <Link to='/about' className='link'>About</Link>
           </li>
           <li>
-            <Link to='/contact'>Contact</Link>
+            <Link to='/contact' className='link'>Contact</Link>
           </li>
         </ul>
+        </div>
         <div className='auth'>
           <CiLogin />
+          <CiLogout />
         </div>
       </div>
     </div>
