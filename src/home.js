960320f6c5aaa2c13960af98f04e39b6b4c71f0b
Busari Moruf <busarimoruf20@gmail.com>
2023-10-05 16:50:21 +0100

    new changes

--- src/home.js
+++ src/home.js
@@ -1,8 +1,22 @@
-import React from 'react'
-
+import React from 'react';
+import { Link } from "react-router-dom";
+import { BsArrowRight } from 'react-icons/bs';
+import './home.css'
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <div className='top_banner'>
+      <div className='container'>
+        <div className='detail'>
+          <h2>The Best Note Book Collection</h2>
+          <Link to='/product' className='link'>Shop Now <BsArrowRight /></Link>
+        </div>
+        <div className='img_box'>
+          <img src='./img/slider-img.png' alt='slidering'></img>
+        </div>
+      </div>
+    </div>
+    </>
   )
 }
 
