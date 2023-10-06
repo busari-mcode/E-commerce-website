04f2f6a8e56446616ab87890e1fc36c41e9939ed
Busari Moruf <busarimoruf20@gmail.com>
2023-10-06 17:07:09 +0100

    new changes

--- src/home.js
+++ src/home.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import { BsArrowRight } from 'react-icons/bs';
+import { BsArrowRight, BsCurrencyDollar } from 'react-icons/bs';
+import { FiTruck } from 'react-icons/fi';
+import { BiHeadphone } from 'react-icons/bi';
+import { CiPercent } from 'react-icons/ci';
 import './home.css'
 const Home = () => {
   return (
@@ -13,6 +16,89 @@ const Home = () => {
         </div>
         <div className='img_box'>
           <img src='./img/slider-img.png' alt='slidering'></img>
+        </div>
+      </div>
+    </div>
+    <div className='product_type'>
+      <div className='container'>
+        <div className='box'>
+          <div className='img_box'>
+            <img src='./img/Mobile Phone.png' alt='mobile'></img>
+          </div>
+          <div className='detail'>
+            <p>23 products</p>
+          </div>
+        </div>
+
+        <div className='box'>
+          <div className='img_box'>
+            <img src='./img/smart watch.png' alt='watch'></img>
+          </div>
+          <div className='detail'>
+            <p>18 products</p>
+          </div>
+        </div>
+
+        <div className='box'>
+          <div className='img_box'>
+            <img src='./img/headphone.png' alt='headphone'></img>
+          </div>
+          <div className='detail'>
+            <p>52 products</p>
+          </div>
+        </div>
+
+        <div className='box'>
+          <div className='img_box'>
+            <img src='./img/cpu.jpg' alt='cpu'></img>
+          </div>
+          <div className='detail'>
+            <p>63 products</p>
+          </div>
+        </div>
+
+      </div>
+    </div>
+    <div className='about'>
+      <div className='container'>
+        <div className='box'>
+          <div className='icon'>
+            <FiTruck />
+          </div>
+          <div className='detail'>
+            <h3>Free shipping</h3>
+            <p>Order above $1000</p>
+          </div>
+        </div>
+
+        <div className='box'>
+          <div className='icon'>
+            <BsCurrencyDollar />
+          </div>
+          <div className='detail'>
+            <h3>Return & Refund</h3>
+            <p>Money back Guaranty</p>
+          </div>
+        </div>
+
+        <div className='box'>
+          <div className='icon'>
+            <CiPercent />
+          </div>
+          <div className='detail'>
+            <h3>Member Discount</h3>
+            <p>On every Order</p>
+          </div>
+        </div>
+
+        <div className='box'>
+          <div className='icon'>
+            <BiHeadphone />
+          </div>
+          <div className='detail'>
+            <h3>Customer Support</h3>
+            <p>Every Time Call Support</p>
+          </div>
         </div>
       </div>
     </div>
