f4108f270a997d026e60ae75a5b5130325ab5fbe
Busari Moruf <busarimoruf20@gmail.com>
2023-10-14 23:22:19 +0100

    new changes

--- src/cart.js
+++ src/cart.js
@@ -1,16 +1,32 @@
 import React from 'react'
-
+import { AiOutlineClose } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
+import './cart.css'
 const Cart = ({cart, setCart}) => {
   return (
     <>
-    <div className='container'>
+    <div className='cartcontainer'>
+        {cart.length === 0 && 
+        <div className='emptycart'>
+        <h2 className='empty'>Cart is Empty</h2>
+        <Link to='/product' className='emptycartbtn'>Shop Now</Link>
+        </div>
+        }
         <div className='contant'>
             {
                 cart.map((curElm) =>
                 {
                     return(
-                        <div className='cart_item'>
-                            <div className='img_box'></div>
+                        <div className='cart_item' key={curElm.id}>
+                            <div className='img_box'>
+                                <img src={curElm.Img} alt={curElm.Title}></img>
+                            </div>
+                            <div className='detail'>
+                                <h4>{curElm.Cat}</h4>
+                                <h3>{curElm.Title}</h3>
+                                <p>{curElm.Price}</p>
+                                <button><AiOutlineClose /></button>
+                            </div>
                         </div>
                     )
                 })
